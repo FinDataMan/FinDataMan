8dc55bb47f2ea8b28487c4cf5c3f3811d980872c
FinDataMan <arturo.villagomezg@gmail.com>
2024-07-23 20:53:01 -0600

    menu responsiveness (missing the options

--- resources/main.js
+++ resources/main.js
@@ -1,6 +1,7 @@
 function responsiveNavColor(scrollPosition) {
     const nav = document.querySelector('nav'); // Select the navigation element
     const logo = document.getElementById('nav-logo'); // Select the img in first div
+    const menu = document.getElementById('nav-menu');
 
     window.addEventListener('scroll', () => {
       const scrolled = window.scrollY; // Get the current scroll position
@@ -8,9 +9,11 @@ function responsiveNavColor(scrollPosition) {
       if (scrolled >= scrollPosition) {
         nav.classList.add('scrolled'); // Add a class for styling changes
         logo.src = './resources/images/blue logo.png';
+        menu.src = './resources/images/menu-blue.png';
       } else {
         nav.classList.remove('scrolled'); // Remove the class if scrolled up
         logo.src = './resources/images/gray logo.png';
+        menu.src = './resources/images/menu-gray.png';
       }
     });
 }
